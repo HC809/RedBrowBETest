2d670600c1dffc1e91010d6c2c35c0b6bcd9e3a1
Hector Caballero <caballero.22.809@gmail.com>
2023-11-20 18:08:56 -0600

    Add DbContext & user entity

---
+++ RedbrowBackendTest/DBContext/TestDBContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using RedbrowBackendTest.Entities;
+
+namespace RedbrowBackendTest.DBContext;
+
+public partial class TestDBContext : DbContext
+{
+    public TestDBContext()
+    {
+    }
+
+    public TestDBContext(DbContextOptions<TestDBContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Usuario> Usuario { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Usuario>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("usuario_pkey");
+
+            entity.ToTable("usuario");
+
+            entity.Property(e => e.Id).HasColumnName("id");
+            entity.Property(e => e.Correo)
+                .HasMaxLength(100)
+                .HasColumnName("correo");
+            entity.Property(e => e.Edad).HasColumnName("edad");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(100)
+                .HasColumnName("nombre");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
